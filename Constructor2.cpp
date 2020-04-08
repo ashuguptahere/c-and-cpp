e990b3bc1bb14b5678e5a5c28803a2a213ab903e
Aashish Gupta <51059360+ash81197@users.noreply.github.com>
2020-04-09 02:46:56 +0530

    Add files via upload

---
+++ Constructor2.cpp
@@ -0,0 +1,22 @@
+#include<iostream.h>
+#include<conio.h>
+class stu
+{
+    inta,b,c;
+    public:
+    stu()
+    {
+        cout<<"Enter the value of a:";
+        cin>>a;
+        cout<<"Enter the value of b:";
+        cin>>b;
+        c=a+b;
+        cout<<"\n Sum is:"<<c;
+    }
+};
+void main()
+{
+    clrscr();
+    stu cv;
+    getch();
+}
