07c0d3443202a51ac4392b29eeb4c4d79fcd5c98
Aashish Gupta <51059360+ash81197@users.noreply.github.com>
2020-04-14 02:23:04 +0530

    Add files via upload

---
+++ Questions/IntegerIntoCharcter.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char ch;
+    //cout<<"Enter any Character: ";
+    //cin>>ch;
+    for(int i=0;i<=256;i++)
+    {
+        //if(i==ch)
+        cout<<i<<" character"<<endl;
+    }
+}
