fc5bbc287083464e93011e88388b2de59e0c0d2e
Aashish Gupta <51059360+ash81197@users.noreply.github.com>
2020-04-14 02:22:51 +0530

    Add files via upload

---
+++ Questions/Hexsum.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+//#include<conio.h>
+//#include<iomanip>
+int main()
+{
+   using namespace std;
+   //clrscr();
+   int a,b,c;
+   cout<<"Enter the value of a & b:";
+   cin>>a>>b;
+   c=a+b;
+   cout<<"c:"<<hex<<c<<"\n";
+   //getch();
+}
