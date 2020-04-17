362616bde0ce51b957e8c71fecbb92004590a62e
Aashish Gupta <51059360+ash81197@users.noreply.github.com>
2020-04-17 23:44:03 +0530

    Add files via upload

---
+++ 3.cpp
@@ -0,0 +1,56 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <cassert>
+using namespace std;
+
+/*Write the class AddElements here*/
+template <class T>
+class AddElements
+{
+    T x;
+    T element;
+    public:
+    AddElements(T element1)
+    {
+        x = element1;
+    }
+    T add(T element2)
+    {
+        return x+element2;
+    }
+    T concatenate(T element2)
+    {
+        return x+element2;
+    }
+};
+
+int main () {
+  int n,i;
+  cin >> n;
+  for(i=0;i<n;i++) {
+    string type;
+    cin >> type;
+    if(type=="float") {
+        double element1,element2;
+        cin >> element1 >> element2;
+        AddElements<double> myfloat (element1);
+        cout << myfloat.add(element2) << endl;
+    }
+    else if(type == "int") {
+        int element1, element2;
+        cin >> element1 >> element2;
+        AddElements<int> myint (element1);
+        cout << myint.add(element2) << endl;
+    }
+    else if(type == "string") {
+        string element1, element2;
+        cin >> element1 >> element2;
+        AddElements<string> mystring (element1);
+        cout << mystring.concatenate(element2) << endl;
+    }
+  }
+  return 0;
+}
