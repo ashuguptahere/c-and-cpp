3c94923c2859934ae15d7da1954f3cbb0132234e
Aashish Gupta <51059360+ash81197@users.noreply.github.com>
2020-04-30 02:35:15 +0530

    Add files via upload

---
+++ 30DaysOfHackerRank/9.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the factorial function below.
+int factorial(int n) {
+    if(n>0)
+        return n * factorial(n-1);
+    else 
+        return 1;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    int result = factorial(n);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
