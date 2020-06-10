481a2c6c05619bfc22ea9289430ac83b15f2c94c
Aashish Gupta <51059360+ash81197@users.noreply.github.com>
2020-06-11 00:21:07 +0530

    Add files via upload

---
+++ Data Structures/1. Linked List/5_LLRecCount.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+}*first;
+
+void create(int A[], int n)
+{
+    struct Node *p, *t, *last;
+    first = (struct Node *)malloc(sizeof(struct Node));
+    first->data = A[0];
+    first->next = NULL;
+    last = first;
+
+    for(int i=1; i<n; i++)
+    {
+        t = (struct Node *)malloc(sizeof(struct Node));
+        t->data = A[i];
+        t->next = NULL;
+        last->next = t;
+        last = t;
+    }
+}
+
+void display(struct Node *n)
+{
+    if(n)
+    {
+        printf("%d ", n->data);
+        display(n->next);
+    }
+}
+
+int count(struct Node *p)
+{
+    if(p == NULL)
+        return 0;
+    else
+        return count(p->next) + 1;
+}
+
+int main()
+{
+    int A[] = {1,2,3,4,5};
+    create(A, 5);
+    display(first);
+    printf("\nNumber of node is: %d", count(first));
+    return 0;
+}
